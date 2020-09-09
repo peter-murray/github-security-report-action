921aea7aa9fc16993c6488c4efb833968e73ce4f
Peter Murray <peter-murray@users.noreply.github.com>
2020-09-09 22:12:32 +0100

    Switching to puppeteer-core

--- src/pdfWriter.js
+++ src/pdfWriter.js
@@ -1,28 +1,30 @@
 const path = require('path')
   , os = require('os')
-  // , pdf = require('html-pdf')
-  , puppeteer = require('puppeteer')
+  , puppeteer = require('puppeteer-core')
   ;
 
 module.exports.save = (html, file) => {
 
-  return puppeteer.launch()
-    .then(browser => {
-      return browser.newPage()
-        .then(page => {
-          return page.setContent(html)
+  const fetcher = puppeteer.createBrowserFetcher();
+  return fetcher.download('782078')
+    .then(revisionInfo => {
+      return puppeteer.launch({executablePath: revisionInfo.executablePath})
+        .then(browser => {
+          return browser.newPage()
+            .then(page => {
+              return page.setContent(html)
+                .then(() => {
+                  return page.pdf({path: file, format: 'A4'})
+                });
+            })
             .then(() => {
-              return page.pdf({path: file, format: 'A4'})
+              return browser.close();
             });
         })
         .then(() => {
-          return browser.close();
+          return file;
         });
-    })
-    .then(() => {
-      return file;
-    })
-
+    });
 }
 
 // module.exports.save = (html, file) => {
