23f0110f4c02b3bc356985c99512b2b57b31ab08
Peter Murray <peter-murray@users.noreply.github.com>
2020-09-09 21:46:27 +0100

    Attempting to use puppeteer

--- src/pdfWriter.js
+++ src/pdfWriter.js
@@ -1,26 +1,48 @@
 const path = require('path')
   , os = require('os')
-  , pdf = require('html-pdf')
+  // , pdf = require('html-pdf')
+  , puppeteer = require('puppeteer')
   ;
 
 module.exports.save = (html, file) => {
-  return new Promise((resolve, reject) => {
-    pdf.create(html, getOptions()).toFile(file, (err, res) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(res);
-      }
+
+  return puppeteer.launch()
+    .then(browser => {
+      return browser.newPage()
+        .then(page => {
+          return page.setContent(html)
+            .then(() => {
+              return page.pdf({path: file, format: 'A4'})
+            });
+        })
+        .then(() => {
+          return browser.close();
+        });
     })
-  });
+    .then(() => {
+      return file;
+    })
+
 }
 
-function getOptions() {
-  const isWindows = os.platform() === 'win32'
-    , executableName = isWindows ? 'phantomjs.exe' : 'phantomjs'
-  ;
-
-  return {
-    phantomPath: path.join(__dirname, '..', 'phantomjs', os.platform(), 'bin', executableName)
-  };
-}
+// module.exports.save = (html, file) => {
+//   return new Promise((resolve, reject) => {
+//     pdf.create(html, getOptions()).toFile(file, (err, res) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(res);
+//       }
+//     })
+//   });
+// }
+//
+// function getOptions() {
+//   const isWindows = os.platform() === 'win32'
+//     , executableName = isWindows ? 'phantomjs.exe' : 'phantomjs'
+//   ;
+//
+//   return {
+//     phantomPath: path.join(__dirname, '..', 'phantomjs', os.platform(), 'bin', executableName)
+//   };
+// }
