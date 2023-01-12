f3d3e34f534c3ae638514d6fb0a46dd7c62ce3a6
jorge-abarca <29783307+jorge-abarca@users.noreply.github.com>
2023-01-12 14:39:57 -0600

    Fixed puppeteer's newPage hanging in Linux
    Modified the puppeteer's launch method to prevent this to hang in Linux, as per: https://stackoverflow.com/a/61050539

--- src/pdf/pdfWriter.ts
+++ src/pdf/pdfWriter.ts
@@ -8,7 +8,18 @@ export function createPDF(html: string, file: string): Promise<string> {
 
   return fetcher.download('782078')//TODO need to store and inject this
     .then(revisionInfo => {
-      return puppeteer.launch({executablePath: revisionInfo.executablePath})
+      return puppeteer.launch({
+          executablePath: revisionInfo.executablePath,
+          headless: true,
+          devtools: true,
+          args: [
+            '--ignore-certificate-errors',
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-accelerated-2d-canvas',
+            '--disable-gpu'
+          ]
+        })
         .then(browser => {
           return browser.newPage()
             .then(page => {
